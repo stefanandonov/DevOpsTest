b319986b3f7e311a4e033cee884eae5bf4cfa527
Stefan Andonov <stefan5andonov@yahoo.com>
2020-02-23 01:03:48 +0100

    "fix on tests"

--- src/main/java/NumbersUtil.java
+++ src/main/java/NumbersUtil.java
@@ -14,7 +14,8 @@ public class NumbersUtil {
     }
 
     public static int countOddNumbers (List<Integer> list) {
-
+        if (list==null)
+            return 0;
         int count = 0;
         for (Integer i : list) {
             if (i%2==1)
